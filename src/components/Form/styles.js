cea092d79a07940ad5204667687b5962758bf734
Daisy <daisyliudesign@gmail.com>
2021-03-10 07:47:29 -0500

    commit

---
+++ src/components/Form/styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    borderRadius: '15px',
+    boxShadow: "0 10px 10px rgb(0 0 0 / 20%), 0 0 50px rgb(0 0 0 / 20%)",
+
+    '&:hover': {
+          boxShadow: '0 0 10px #fff, -10px 0 30px #F6F50A, 10px 0 10px #F6F50A'
+    }
+    
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    
+  },
+  fileInput: {
+    width: '97%',
+    margin: '10px 0',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
---
+++ src/components/Posts/Post/styles.js
@@ -0,0 +1,60 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundBlendMode: 'darken',
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderRadius: '15px',
+    height: '100%',
+    position: 'relative',
+    boxShadow: "0 10px 10px rgb(0 0 0 / 20%), 0 0 50px rgb(0 0 0 / 20%)",
+
+    '&:hover': {
+          boxShadow: '0 0 10px #fff, -10px 0 30px #F6F50A, 10px 0 10px #F6F50A'
+    }
+
+
+  },
+
+  overlay: {
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+    color: 'white',
+  },
+  overlay2: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+    color: 'white',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
