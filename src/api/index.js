22b0cd8242b2f0cfe207402ee01fa2f7a0c856b9
Daisy <daisyliudesign@gmail.com>
2021-03-12 00:25:06 -0500

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,17 @@ import axios from "axios";
 
 const API = axios.create({baseURL:'https://dream-machine-daisyliu.herokuapp.com'})
 
+// const API = axios.create({baseURL:'http//localhost:5000'})
 
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+      req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+  
+    return req;
+  });
+
+  
 
 export const fetchPosts = () => API.get('/posts');
 
@@ -15,6 +25,6 @@ export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
-export const signIn= (formData)=>API.post('/users/signin',formData);
+export const signIn = (formData) => API.post('/user/signin', formData);
 
-export const signUp= (formData)=>API.post('/users/signup',formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
