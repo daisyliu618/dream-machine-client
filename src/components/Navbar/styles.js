0987033bbb607a26f7b4319997aeb8f04953a49d
Daisy <daisyliudesign@gmail.com>
2021-03-13 20:52:48 -0500

    commit

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,26 +1,26 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: "30px 0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "10px 50px",
-  },
+  // appBar: {
+  //   borderRadius: 15,
+  //   margin: "30px 0",
+  //   display: "flex",
+  //   flexDirection: "row",
+  //   justifyContent: "space-between",
+  //   alignItems: "center",
+  //   padding: "10px 50px",
+  // },
   toolbar: {
     display: "flex",
     justifyContent: "flex-end",
-    width: "400px",
+    // width: "400px",
     minHeight: "40px",
     padding: "5px",
     marginBottom: "25px",
     background: "rgba(36, 209, 215, 0.75)",
     borderRadius: "5px",
     boxShadow: "0 5px 5px rgb(0 0 0 / 20%), 0 0 20px rgb(0 0 0 / 20%)",
-    marginLeft: '1.5rem'
+    marginTop: '2rem'
   },
   profile: {
     display: "flex",
